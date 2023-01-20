26eb77d4953334dc8cb423b9b4056f489ae346aa
SofiaCurreri <sofiacurreri@gmail.com>
2023-01-20 15:48:11 +0100

    esercizio completato

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let user_name = prompt("Qual è il tuo nome?");
+let user_surname = prompt("Qual è il tuo cognome?");
+let fav_color = prompt("Qual è il tuo colore preferito?");
+
+document.getElementById('frase').innerHTML = user_name + user_surname + fav_color + "23";
