63d85e21015d4ba8e654eb51e4a0a88ee97b6f7e
SofiaCurreri <sofiacurreri@gmail.com>
2023-01-23 16:22:49 +0100

    aggiunto messaggio "La tua password è "

--- js/main.js
+++ js/main.js
@@ -2,4 +2,4 @@ let user_name = prompt("Qual è il tuo nome?");
 let user_surname = prompt("Qual è il tuo cognome?");
 let fav_color = prompt("Qual è il tuo colore preferito?");
 
-document.getElementById('frase').innerHTML = user_name + user_surname + fav_color + "23";
+document.getElementById('frase').innerHTML = "La tua password è: " + user_name + user_surname + fav_color + "23";
