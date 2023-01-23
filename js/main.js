f9ce176fce08a7fdb464a9020f8076d05106b591
SofiaCurreri <sofiacurreri@gmail.com>
2023-01-23 17:06:53 +0100

    variabili impostate come const

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-let user_name = prompt("Qual è il tuo nome?");
-let user_surname = prompt("Qual è il tuo cognome?");
-let fav_color = prompt("Qual è il tuo colore preferito?");
+const user_name = prompt("Qual è il tuo nome?");
+const user_surname = prompt("Qual è il tuo cognome?");
+const fav_color = prompt("Qual è il tuo colore preferito?");
 
 document.getElementById('frase').innerHTML = "La tua password è: " + user_name + user_surname + fav_color + "23";
